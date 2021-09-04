a5fead2042ee13ba1ded15ba5765f41451bdcfe5
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-09-04 17:58:22 -0500

    Story 2 for Lab 1

--- labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
@@ -12,7 +12,25 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameStart();
+        }
+
+        static void GameStart ()
+        {
+            Console.WriteLine("Mitchel Campozano, ITSE 1430 Adventure Game, Fall 2021");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("A clap of thunder wakes you from your unconscious, rain sweeping across the dead, charred land around you.");
+            Console.WriteLine("The raindrops tap against your armor as you look around at the unfamiliar forest.");
+            Console.WriteLine("Dead trees loom over you as you wander through the strange woods that eventually give way to a clearing in front of a temple.");
+            Console.WriteLine("Large bronze doors tower over you, embossed with a tale of struggle between man and deities.");
+            Console.WriteLine("Before you can move, they groan open, cracking enough to give you room to enter.");
+            Console.WriteLine("Do you venture forward, adventurer?");
+            Console.WriteLine("");
+
+            string input = Console.ReadLine();
+
+
         }
     }
 }
