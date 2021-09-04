0c4f65c7991948df6a3e59fab52ea730b25341d5
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-09-04 17:16:56 -0500

    Getting started on Lab 1 (Story 1)

---
+++ labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Adventure Game Lab
+ * Mitchel Campozano
+ * ITSE 1430 Fall 2021
+*/
+
+using System;
+
+namespace MitchelCampozano.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
