ed03d40a33efe1636603dd19d5132b7232f3ec85
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-09-04 19:00:05 -0500

    Story 3 in progress

--- labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
@@ -12,9 +12,21 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
     {
         static void Main(string[] args)
         {
+            string choice;
+
             GameStart();
+
+            // TODO : Finish Game Loop
+            while (!exitGame())
+            {
+                choice = CheckMove("What would you like to do, adventurer?", true);
+
+                if (choice == "quit")
+                    exitGame();
+            } 
         }
 
+       
         static void GameStart ()
         {
             Console.WriteLine("Mitchel Campozano, ITSE 1430 Adventure Game, Fall 2021");
@@ -25,12 +37,33 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
             Console.WriteLine("Dead trees loom over you as you wander through the strange woods that eventually give way to a clearing in front of a temple.");
             Console.WriteLine("Large bronze doors tower over you, embossed with a tale of struggle between man and deities.");
             Console.WriteLine("Before you can move, they groan open, cracking enough to give you room to enter.");
-            Console.WriteLine("Do you venture forward, adventurer?");
             Console.WriteLine("");
+        }
+
+        static string CheckMove ( string message, bool checkVal )
+        {
+            Console.WriteLine(message);
 
             string input = Console.ReadLine();
 
+            // TODO: Input validation for various commands
+            return input;
+        } 
+        static bool exitGame()
+        {
+            string quitCom;
 
+            Console.WriteLine("Are you sure you want to quit? (Y / N)");
+            quitCom = Console.ReadLine();
+
+            if (quitCom == "n")
+                return false;
+            else if (quitCom == "y")
+                return true;
+            else
+                Console.WriteLine("Invalid entry.");
+            return default;
         }
+
     }
 }
