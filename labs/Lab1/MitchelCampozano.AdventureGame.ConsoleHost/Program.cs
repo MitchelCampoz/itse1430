d952381843d99cffccd01380de3763fca6a880a6
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-09-16 16:49:33 -0500

    Fit the player into some sort of box. Play tracking working just not correctly

--- labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/MitchelCampozano.AdventureGame.ConsoleHost/Program.cs
@@ -27,10 +27,6 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
             } while (!done);
         }
 
-        // Maximum value for rooms based on XY axes
-        static int maximumX = 3;
-        static int maximumY = 3;
-
         // Player coordinates
         static int placeX;
         static int placeY;
@@ -93,23 +89,33 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
                 break;
                 case "movewest": newX -= 1;// WIP
                 break;
+                case "stand": break;
                 default: ErrorMessage("Invalid Move"); 
                 break;
 
             };
 
-            if (placeX  >= 0 && placeX  < maximumX && placeY >= 0 && placeY < maximumY)
+            placeX = newX; 
+            placeY = newY;
+
+            if (placeX  >= 0 && placeX  < 3 && placeY >= 0 && placeY < 3)
             {
-                int roomNumber = placeX + (maximumX * (placeY - 1));
+                int roomNumber = placeX + (3 * (placeY - 1));
 
                 switch (roomNumber)
                 {
                     case 1: Room1(); break;
                     case 2: Room2(); break;
                     case 3: Room3(); break;
+                    case 4: Room4(); break;
+                    case 5: Room5(); break;
+                    case 6: Room6(); break;
+                    case 7: Room7(); break;
+                    case 8: Room8(); break;
+                    case 9: Room9(); break;
                 };
             } else
-                Console.WriteLine("You bonk into the wall");
+                ErrorMessage("You bonk into the wall");
         }
 
         // Inspect function
@@ -158,104 +164,48 @@ namespace MitchelCampozano.AdventureGame.ConsoleHost
 
         static void Room1 ()
         {
-            Console.WriteLine("You found me");
+            Console.WriteLine("You found room1");
         }
 
         static void Room2 ()
         {
-            Console.WriteLine("You found me");
+            Console.WriteLine("You found room2");
         }
 
         static void Room3 ()
         {
-            Console.WriteLine("You found me");
+            Console.WriteLine("You found room3");
         }
-        //static void RoomSouthWest ()
-        //{
 
-        //    // Has one way to go (North)
+        static void Room4 ()
+        {
+            Console.WriteLine("You found room4");
+        }
 
-        //    // The bodies here are not as armoured as the dead ones before you, but they still carry weapons
-        //    // They surround a chained metal chest
-        //    // You can't access the chest as it is chained with a lock
-        //    Console.WriteLine("You enter the room, seeing a slew of light troops laying on the floor.");
-        //    Console.WriteLine("At the center of the room, a large chest sits upright, chains and soldiers alike wrapped around it.");
-        //    Console.WriteLine("In the middle of the chest, there is a lock.");
+        static void Room5 ()
+        {
+            Console.WriteLine("You found room5");
+        }
 
-        //    // Going North takes you to RoomWest()
-        //}
+        static void Room6 ()
+        {
+            Console.WriteLine("You found room6");
+        }
 
-        //static void RoomWest ()
-        //{
-        //    // Has two ways to go (East and South)
-        //    // The bodies here are stacked all near the doorway to the next room
-        //    Console.WriteLine("Your eyes adjust to the sickly dark as you enter what appears to be a dining hall.");
-        //    Console.WriteLine("Corpses are strewn about, all furniture either tossed with them or shattered in pieces.");
-        //    Console.WriteLine("Tapestries hang tattered, one draped down on the floor like a red carpet into the only other doorway aside from the one you entered.");
-        //    Console.WriteLine("Surrounding this doorway are many soldiers piled high.");
+        static void Room7 ()
+        {
+            Console.WriteLine("You found room7");
+        }
 
-        //    // Going East goes to RoomCentral()
+        static void Room8 ()
+        {
+            Console.WriteLine("You found room8");
+        }
 
-        //    // Going South goes to RoomSouthWest()
-        //}
+        static void Room9 ()
+        {
+            Console.WriteLine("You found room9");
+        }
 
-        //static void RoomNorthWest ()
-        //{
-
-        //}
-
-        //static void RoomSouth ()
-        //{
-        //    // First room to go in
-        //    // Has one way to go (North)
-        //    // Stacked with bodies soldiers wearing different armour than you
-
-        //    // Going North takes you to RoomCentral()
-        //}
-
-        //static void RoomCentral ()
-        //{
-        //    // Has three ways to go (West and East and South)
-        //    // More bodies, but now you can hear faint growling
-
-        //    // Going West goes to RoomWest()
-
-        //    // Going East goes to RoomEast()
-        //}
-
-        //static void RoomNorth ()
-        //{
-
-        //}
-
-        //static void RoomSouthEast ()
-        //{
-        //    // Has one way to go (North)
-        //    // Room is surprisingly empty, with but one body cowered in the corner
-        //    // Its armour is more ornate than the ones you've found thus far, it must be their superior
-        //    // In its left hand is a vial that seems to have contained poison
-        //    // In its right hand, held close and stiff to its chest, is a key
-
-        //    // Take Key
-
-        //    // Going North takes you to RoomEast()
-        //}
-
-        //static void RoomEast ()
-        //{
-        //    // Has three ways to go (North and South and West)
-        //    // This room has heavily armored bodies strewn about the room
-        //    // There is a large blood streak leading North
-        //    // The growling is growing heavier
-
-        //    // Going West takes you to RoomCentral()
-        //    // Going South takes you to RoomSouthEast()
-        //    // Going North takes you to RoomNorthEast()
-        //}
-
-        //static void RoomNorthEast ()
-        //{
-
-        //}
     }
 }
