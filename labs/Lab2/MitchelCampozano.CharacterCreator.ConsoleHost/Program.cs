76aae12ff1711877793735aa25c3d36eb820a7d1
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-09-30 15:26:58 -0500

    Beginning Lab 2 (fingers crossed)

--- labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
@@ -9,24 +9,41 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Mitchel Campozano, ITSE 1430 Adventure Game, Fall 2021");
             Console.WriteLine("------------------------------------------------------");
             Console.ResetColor();
+
+            bool done = false;
+
+            do
+            {
+                char choice = GetInput();
+
+                switch (choice)
+                {
+                    case 'A': AddCharacter(); break;
+                    case 'B': break;
+                    case 'C': break;
+                    case 'Q': done = CheckQuit(); break;
+                    default: ErrorMessage("Unknown choice"); break;
+                };
+            } while (!done);
         }
 
-        static char MainMenu ()
+        static char GetInput ()
         {
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("");
                 Console.WriteLine("Please select an option: ");
-                Console.WriteLine("A. ");
+                Console.WriteLine("A) dd a Character ");
                 Console.WriteLine("B. ");
                 Console.WriteLine("C. ");
-                Console.WriteLine("D. ");
+                Console.WriteLine("Q) uit ");
                 Console.ResetColor();
 
                 string choice = Console.ReadLine().Trim();
@@ -36,10 +53,48 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
                     case "A": return 'A';
                     case "B": return 'B';
                     case "C": return 'C';
-                    case "D": return 'D';
-                    default: Console.WriteLine("Wrong choice"); break;
+                    case "Q": return 'Q';
                 }
+
+                ErrorMessage("Invalid choice.");
             };
+        }
+
+        static void AddCharacter ()
+        {
+            do
+            {
+                var newCharacter = new Character();
+            } while (true);
+        }
+
+        static bool CheckQuit ()
+        {
+            if (ReadBoolean("Are you sure you want to quit? (Y/N)"))
+                return true;
+
+            return false;
+        }
+
+        static bool ReadBoolean ( string message )
+        {
+            Console.Write(message);
+
+            do
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Y)
+                    return true;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
+            } while (true);
+        }
+
+        static void ErrorMessage ( string message )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
---
+++ labs/Lab2/MitchelCampozano.CharacterCreator/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace MitchelCampozano.CharacterCreator
+{
+    public class Character
+    {
+        /// <summary>
+        /// Gets and sets the name of the character
+        /// </summary>
+        public string Name
+        {
+            get { return _name ?? ""; }
+
+            set { _name = value?.Trim(); }
+        }
+
+        /// <summary>
+        /// Gets and sets the profession or class of the character
+        /// </summary>
+        public string Profession
+        {
+            get { return (_profession != null) ? _profession : ""; }
+
+            set { _profession = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>
+        /// Gets and sets the race of the character
+        /// </summary>
+        public string Race
+        {
+            get { return _race ?? ""; }
+
+            set { _race = value?.Trim(); }
+        }
+
+        /// <summary>
+        /// Gets and sets the potentially tragic backstory of the character
+        /// </summary>
+        public string Biography
+        {
+            get { return (_biography != null) ? _biography : ""; }
+
+            set { _biography = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>
+        /// Gets and sets the Strength of the character
+        /// </summary>
+        public int Strength { get; set; }
+
+        /// <summary>
+        /// Gets and sets the Intelligence of the character
+        /// </summary>
+        public int Intelligence { get; set; }
+
+        /// <summary>
+        /// Gets and sets the Agility of the character
+        /// </summary>
+        public int Agility { get; set; }
+
+        /// <summary>
+        /// Gets and sets the Constitution
+        /// </summary>
+        public int Constitution { get; set; }
+
+        /// <summary>
+        /// Gets and sets the Charisma of the character
+        /// </summary>
+        public int Charisma { get; set; }
+
+        private string _name;
+        private string _profession;
+        private string _race;
+        private string _biography;
+
+        private int _strength;
+        private int _intelligence;
+        private int _agility;
+        private int _constitution;
+        private int _charisma;
+
+        public string AttributeChecker ()
+        {
+            if (Strength < 1 || Strength > 100)
+                return "Invalid amount.";
+
+            if (Intelligence < 1 || Intelligence > 100)
+                return "Invalid amount.";
+
+            if (Agility < 1 || Agility > 100)
+                return "Invalid amount.";
+
+            if (Constitution < 1 || Constitution > 100)
+                return "Invalid amount.";
+
+            if (Charisma < 1 || Charisma > 100)
+                return "Invalid amount.";
+
+            return null;
+        }
+    }
+}
