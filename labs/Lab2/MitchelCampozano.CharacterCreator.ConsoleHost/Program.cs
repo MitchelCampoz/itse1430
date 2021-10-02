fd8bfda061d34941b1c43ee9daa5ca26b8adda89
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-10-02 16:37:26 -0500

    Test 123

--- labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
@@ -18,6 +18,8 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
 
             bool done = false;
 
+            // Test comment
+
             do
             {
                 char choice = GetInput();
