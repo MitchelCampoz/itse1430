b934c24ea4803ae9a5d1f03f9f6894acc38b7410
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-10-03 21:08:26 -0500

    Finished rough draft. Going to go through and color everything.

--- labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/MitchelCampozano.CharacterCreator.ConsoleHost/Program.cs
@@ -29,6 +29,7 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
                     case 'A': AddCharacter(); break;
                     case 'V': ViewCharacter(); break;
                     case 'E': EditCharacter(); break;
+                    case 'D': DeleteCharacter(); break;
                     case 'Q': done = CheckQuit(); break;
                     default: ErrorMessage("Unknown choice"); break;
                 };
@@ -47,6 +48,7 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
                 Console.WriteLine("A) dd a Character ");
                 Console.WriteLine("V) iew your Character ");
                 Console.WriteLine("E) dit your Character ");
+                Console.WriteLine("D) elete your Character ");
                 Console.WriteLine("Q) uit ");
                 Console.ResetColor();
 
@@ -57,6 +59,7 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
                     case "A": return 'A';
                     case "V": return 'V';
                     case "E": return 'E';
+                    case "D": return 'D';
                     case "Q": return 'Q';
                 }
 
@@ -130,7 +133,7 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
                 {
                     switch (input.ToUpper())
                     {
-                        case "WARRIOR": return "WARRIOR";
+                        case "WARRIOR": return "Warrior";
                         case "CLERIC": return "Cleric";
                         case "RANGER": return "Ranger";
                         case "ROGUE": return "Rogue";
@@ -182,6 +185,8 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
             {
                 string input = Console.ReadLine().Trim();
 
+                Console.WriteLine("");
+
                 if (!String.IsNullOrEmpty(input) || !checker)
                     return input;
 
@@ -208,10 +213,13 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
             Console.WriteLine($"Agility: {character.Agility}");
             Console.WriteLine($"Constitution: {character.Constitution}");
             Console.WriteLine($"Charisma: {character.Charisma}");
+            Console.WriteLine("");
         }
 
         static void EditCharacter()
         {
+            bool finished = false;
+
             if (character == null)
             {
                 Console.WriteLine("There is no character to edit yet.");
@@ -224,22 +232,44 @@ namespace MitchelCampozano.CharacterCreator.ConsoleHost
             {
                 Console.WriteLine("Here is your current character's information: ");
                 ViewCharacter();
-                Console.WriteLine("What would you like to change?");
+                Console.Write("What would you like to change? (You may enter \"Done\" to quit)");
                 string input = Console.ReadLine();
 
-                switch (input.ToUpper())
+                if (!String.IsNullOrEmpty(input))
                 {
-                    case "NAME": character.Name = NamePicker("Pick a new name: "); break;
-                    case "PROFESSION": character.Profession = JobPicker("Pick a new profession: "); break;
-                    case "RACE": character.Race = RacePicker("Pick a new race: "); break;
-                    case "BIOGRAPHY": character.Biography = BiographyEntry("Enter a biography for your character: ", false); break;
-                    case "STRENGTH": character.Strength = ReadInt32("Enter a new Strength value: ", Character.MinimumValue, Character.MaximumValue); break;
-                    case "INTELLIGENCE": character.Intelligence = ReadInt32("Enter a new Intelligence value: ", Character.MinimumValue, Character.MaximumValue); break;
-                    case "AGILITY": character.Agility = ReadInt32("Enter a new Agility value: ", Character.MinimumValue, Character.MaximumValue); break;
-                    case "CONSTITUTION": character.Constitution = ReadInt32("Enter a new Constitution value: ", Character.MinimumValue, Character.MaximumValue); break;
-                    case "CHARISMA": character.Charisma = ReadInt32("Enter a new Charisma value: ", Character.MinimumValue, Character.MaximumValue); break;
-                }       
-            } while (true);
+                    switch (input.ToUpper())
+                    {
+                        case "NAME": character.Name = NamePicker("Pick a new name: "); break;
+                        case "PROFESSION": character.Profession = JobPicker("Pick a new profession: "); break;
+                        case "RACE": character.Race = RacePicker("Pick a new race: "); break;
+                        case "BIOGRAPHY": character.Biography = BiographyEntry("Enter a biography for your character: ", false); break;
+                        case "STRENGTH": character.Strength = ReadInt32("Enter a new Strength value: ", Character.MinimumValue, Character.MaximumValue); break;
+                        case "INTELLIGENCE": character.Intelligence = ReadInt32("Enter a new Intelligence value: ", Character.MinimumValue, Character.MaximumValue); break;
+                        case "AGILITY": character.Agility = ReadInt32("Enter a new Agility value: ", Character.MinimumValue, Character.MaximumValue); break;
+                        case "CONSTITUTION": character.Constitution = ReadInt32("Enter a new Constitution value: ", Character.MinimumValue, Character.MaximumValue); break;
+                        case "CHARISMA": character.Charisma = ReadInt32("Enter a new Charisma value: ", Character.MinimumValue, Character.MaximumValue); break;
+                        case "DONE": finished = true; break;
+                        default: ErrorMessage("Please enter a valid option."); break;
+                    }
+                } else
+                    ErrorMessage("Please enter a valid option.");
+            } while (!finished);
+        }
+
+        static void DeleteCharacter ()
+        {
+            if (character == null)
+                ErrorMessage("You have no character to delete!");
+            
+            if(character != null)
+            {
+                if (ReadBoolean("Are you sure you want to delete your character? (Y/N)")) 
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Your character's existence has ceased!");
+                    character = null;
+                }
+            }
         }
 
         static bool CheckQuit ()
