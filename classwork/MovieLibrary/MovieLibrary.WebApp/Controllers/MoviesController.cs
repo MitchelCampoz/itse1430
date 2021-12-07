fdba45e98f6675c480ffc7d28e6c316b09b68bda
MitchelCampoz <mitchel.campozano@my.tccd.edu>
2021-12-06 21:22:52 -0600

    Learning Web implementation

---
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieLibrary.WebApp.wwwroot
+{
+    public class MoviesController : Controller
+    {
+        public MoviesController ( IMovieDatabase database)
+        {
+            _database = database;
+        }
+
+        public IActionResult List ()
+        {
+            var movies = _database.GetAll().OrderBy(x => x.Title);
+
+            return View(movies);
+        }
+
+        private readonly IMovieDatabase _database;
+    }
+}
